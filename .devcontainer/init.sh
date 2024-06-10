711438ba54b672b73b0faea25157e73b798f522a
Leonardo Campos <78708394+leonardo8787@users.noreply.github.com>
2024-06-09 22:55:03 -0300

    Update init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Clonar os repositórios
-git clone https://github.com/leonardo8787/Coletor.git /app/Coletor
-git clone https://github.com/recicleUSP/Donor.git /app/Donor
-git clone https://github.com/recicleUSP/manager.git /app/manager
+git clone https://github.com/recicleplusplus/Coletor.git /app/Coletor
+git clone https://github.com/recicleplusplus/Donor.git /app/Donor
+git clone https://github.com/recicleplusplus/manager.git /app/manager
 
 # Instalar dependências npm
 cd /app/Coletor
@@ -14,10 +14,6 @@ npm i --force
 
 cd /app/manager
 npm i --force
-
-# Mount USB drive
-mkdir /mnt/g
-mount -t drvfs G: /mnt/g
 
 # Iniciar o bash
 /bin/bash
